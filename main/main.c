6a700ec55e63e16f8fae50ce94f3da775884f8ef
Honghui-Li-8 <honghuili8@gmail.com>
2024-05-10 14:50:24 -0700

    ble_mesh code .h file

--- main/main.c
+++ main/main.c
@@ -1,5 +1,5 @@
 #include "board.h"
-#include "ble_mesh_config_edge.c"
+#include "ble_mesh_config_edge.h"
 #include "../Secret/NetworkConfig.h"
 
 #define TAG_M "MAIN"
@@ -7,6 +7,11 @@
 
 static void prov_complete_handler(uint16_t node_index, const esp_ble_mesh_octet16_t uuid, uint16_t addr, uint8_t element_num, uint16_t net_idx) {
     ESP_LOGI(TAG_M, " ----------- prov_complete handler trigered -----------");
+}
+
+static void config_complete_handler(uint16_t addr) {
+    ESP_LOGI(TAG_M,  " ----------- Node-0x%04x config_complete -----------", addr);
+    uart_sendMsg(NULL,  " ----------- config_complete -----------");
 }
 
 static void recv_message_handler(esp_ble_mesh_msg_ctx_t *ctx, uint16_t length, uint8_t *msg_ptr) {
@@ -155,11 +160,13 @@ static void rx_task(void *arg)
 
 void app_main(void)
 {
-    // turn off log
-    // esp_log_level_set(TAG_ALL, ESP_LOG_NONE);
-    // uart_sendMsg(TAG_M, "[UART] Turning off all Log's from esp_log\n");
+    // turn off log - important, bc the server counting on '[E]' as end of message instaed of '\0'
+    //              - since the message from uart carries data
+    //              - use uart_sendMsg or uart_sendData for message, the esp_log for dev debug
+    esp_log_level_set(TAG_ALL, ESP_LOG_NONE);
+    uart_sendMsg(TAG_M, "[Ignore_prev][UART] Turning off all Log's from esp_log\n");
 
-    esp_err_t err = esp_module_edge_init(prov_complete_handler, recv_message_handler, recv_response_handler, timeout_handler);
+    esp_err_t err = esp_module_edge_init(prov_complete_handler, config_complete_handler, recv_message_handler, recv_response_handler, timeout_handler);
     if (err != ESP_OK) {
         ESP_LOGE(TAG_M, "Network Module Initialization failed (err %d)", err);
         return;
