bc0a5f43f1901c130a1ea646db7f0ffda1b17eb2
Honghui-Li-8 <honghuili8@gmail.com>
2024-05-31 01:57:45 -0700

    disable uart readintg related log

--- main/main.c
+++ main/main.c
@@ -85,13 +85,15 @@ static void broadcast_handler(esp_ble_mesh_msg_ctx_t *ctx, uint16_t length, uint
     // {
     //     return; // is root's own broadcast
     // }
-    
+
     uint16_t node_addr = ctx->addr;
-    ESP_LOGW(TAG_M, "-> Received Broadcast Message \'%s\' from node-%d", (char *)msg_ptr, node_addr);
+    ESP_LOGE(TAG_M, "-> Received Broadcast Message \'%s\' from node-%d", (char *)msg_ptr, node_addr);
 
     // ========== General case, pass up to APP level ==========
     // pass node_addr & data to to edge device using uart
+    send_message(ctx->addr, length, msg_ptr); // testing log
     uart_sendData(node_addr, msg_ptr, length);
+    ESP_LOGE(TAG_M, "=== Handled Broadcast Message \'%s\' from node-%d ===", (char *)msg_ptr, node_addr);
 }
 
 static void connectivity_handler(esp_ble_mesh_msg_ctx_t *ctx, uint16_t length, uint8_t *msg_ptr) {
@@ -113,11 +115,12 @@ static void connectivity_handler(esp_ble_mesh_msg_ctx_t *ctx, uint16_t length, u
 
     return;
 }
+
 static void execute_uart_command(char* command, size_t cmd_len) {
     size_t cmd_len_raw = cmd_len;
 
-    ESP_LOGI(TAG_M, "execute_command called - %d byte raw - %d decoded byte", cmd_len_raw, cmd_len);
-    uart_sendMsg(0, "Executing command\n");
+    // ESP_LOGI(TAG_M, "execute_command called - %d byte raw - %d decoded byte", cmd_len_raw, cmd_len);
+    // uart_sendMsg(0, "Executing command\n");
 
     static const char *TAG_E = "EXE";
     // static uint8_t *data_buffer = NULL;
@@ -133,8 +136,8 @@ static void execute_uart_command(char* command, size_t cmd_len) {
     // uart command format
     // TB Finish, TB Complete
     if (cmd_len < 5) {
-        ESP_LOGE(TAG_E, "Command [%s] with %d byte too short", command, cmd_len);
-        uart_sendMsg(0, "Command too short\n");
+        // ESP_LOGE(TAG_E, "Command [%s] with %d byte too short", command, cmd_len);
+        // uart_sendMsg(0, "Command too short\n");
         return;
     }
     const size_t CMD_LEN = 5;
@@ -161,10 +164,10 @@ static void execute_uart_command(char* command, size_t cmd_len) {
         send_message(node_addr, msg_length, (uint8_t *) msg_start);
         ESP_LOGW(TAG_M, "<- Sended Message \'%.*s\' to node-%d", msg_length, (char*) msg_start, node_addr);
     } else {
-        ESP_LOGE(TAG_E, "Command not Vaild");
+        // ESP_LOGE(TAG_E, "Command not Vaild");
     }
     
-    ESP_LOGI(TAG_E, "Command [%.*s] executed", cmd_len, command);
+    // ESP_LOGI(TAG_E, "Command [%.*s] executed", cmd_len, command);
 }
 
 // static void print_bytes(char* data, size_t length) {
@@ -187,7 +190,7 @@ static void uart_task_handler(char *data) {
         }else if (data[i] == 0xFE) {
             // located end of message
             cmd_end = i; // 0xFE byte
-            uart_sendMsg(0, "Found End of Message!!\n");
+            // uart_sendMsg(0, "Found End of Message!!\n");
         }
 
         if (cmd_end > cmd_start) {
@@ -195,7 +198,7 @@ static void uart_task_handler(char *data) {
             uint8_t* command = (uint8_t *) (data + cmd_start);
             cmd_len = cmd_end - cmd_start;
             cmd_len = uart_decoded_bytes(command, cmd_len, command); // decoded cmd will be put back to command pointer
-            ESP_LOGE("Decoded Data", "i:%d, cmd_start:%d, cmd_len:%d", i, cmd_start, cmd_len);
+            // ESP_LOGE("Decoded Data", "i:%d, cmd_start:%d, cmd_len:%d", i, cmd_start, cmd_len);
 
             execute_uart_command(data + cmd_start, cmd_len); //TB Finish, don't execute at the moment
         }
@@ -203,8 +206,8 @@ static void uart_task_handler(char *data) {
 
     if (cmd_start > cmd_end) {
         // one message is only been read half into buffer, edge case. Not consider at the moment
-        ESP_LOGE("E", "Buffer might have remaining half message!! cmd_start:%d, cmd_end:%d", cmd_start, cmd_end);
-        uart_sendMsg(0, "[Error] Buffer might have remaining half message!!\n");
+        // ESP_LOGE("E", "Buffer might have remaining half message!! cmd_start:%d, cmd_end:%d", cmd_start, cmd_end);
+        // uart_sendMsg(0, "[Error] Buffer might have remaining half message!!\n");
     }
 }
 
@@ -215,7 +218,7 @@ static void rx_task(void *arg)
     static const char *RX_TASK_TAG = "RX";
     uint8_t* data = (uint8_t*) malloc(UART_BUF_SIZE + 1);
     ESP_LOGW(RX_TASK_TAG, "rx_task called ------------------");
-    esp_log_level_set(TAG_ALL, ESP_LOG_NONE);
+    // esp_log_level_set(TAG_ALL, ESP_LOG_NONE);
 
     while (1) {
         const int rxBytes = uart_read_bytes(UART_NUM, data, UART_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
@@ -234,9 +237,10 @@ void app_main(void)
     // turn off log - important, bc the server counting on '[E]' as end of message instaed of '\0'
     //              - since the message from uart carries data
     //              - use uart_sendMsg or uart_sendData for message, the esp_log for dev debug
-    esp_log_level_set(TAG_ALL, ESP_LOG_NONE);
-    uart_sendMsg(0, "[Ignore_prev][UART] Turning off all Log's from esp_log\n");
+    // esp_log_level_set(TAG_ALL, ESP_LOG_NONE);
+    // uart_sendMsg(0, "[Ignore_prev][UART] Turning off all Log's from esp_log\n");
 
+    ESP_LOGI(TAG_M, " ----------- staringing -----------");
     esp_err_t err = esp_module_edge_init(prov_complete_handler, config_complete_handler, recv_message_handler, recv_response_handler, timeout_handler, broadcast_handler, connectivity_handler);
     if (err != ESP_OK) {
         ESP_LOGE(TAG_M, "Network Module Initialization failed (err %d)", err);
@@ -246,4 +250,5 @@ void app_main(void)
     xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 1, NULL);
 
     ESP_LOGI(TAG_M, " ----------- app_main done -----------");
+    // uart_sendMsg(0, "[UART] ----------- app_main done -----------\n");
 }
