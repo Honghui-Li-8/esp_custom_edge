7308273f40bca461d381bedc95d7c0e4b27df494
Honghui-Li-8 <honghuili8@gmail.com>
2024-06-13 01:16:56 -0700

    minimum length updated to 1 byte

--- main/ble_mesh_config_edge.c
+++ main/ble_mesh_config_edge.c
@@ -94,8 +94,8 @@ static esp_ble_mesh_model_op_t client_op[] = { // operation client will "RECEIVE
 };
 
 static esp_ble_mesh_model_op_t server_op[] = { // operation server will "RECEIVED"
-    ESP_BLE_MESH_MODEL_OP(ECS_193_MODEL_OP_MESSAGE, 2),
-    ESP_BLE_MESH_MODEL_OP(ECS_193_MODEL_OP_BROADCAST, 2),
+    ESP_BLE_MESH_MODEL_OP(ECS_193_MODEL_OP_MESSAGE, 1),
+    ESP_BLE_MESH_MODEL_OP(ECS_193_MODEL_OP_BROADCAST, 1),
     ESP_BLE_MESH_MODEL_OP(ECS_193_MODEL_OP_CONNECTIVITY, 1),
     ESP_BLE_MESH_MODEL_OP(ECS_193_MODEL_OP_SET_TTL, 1), // edge will recive set ttl from root
     ESP_BLE_MESH_MODEL_OP_END,
@@ -320,7 +320,7 @@ void send_message(uint16_t dst_address, uint16_t length, uint8_t *data_ptr, bool
     if (require_response) {
         opcode = ECS_193_MODEL_OP_MESSAGE_R;
     }
-    
+
     setNodeState(WORKING);
     err = esp_ble_mesh_client_model_send_msg(client_model, &ctx, opcode, length, data_ptr, MSG_TIMEOUT, require_response, message_role);
     if (err != ESP_OK) {
